82e40c328857e1d696c785f2567e0bcdfd9c8723
Aiteruu <42684804+Aiteruu@users.noreply.github.com>
2020-10-22 17:21:33 -0400

    add better callbacks

--- depth.py
+++ depth.py
@@ -8,15 +8,33 @@ batch_size = 8
 learning_rate = 0.0001
 epochs = 10
 
-model = Depth()
 length, data_generator = batch(8)
 
-model.compile(loss=loss, optimizer=tf.keras.optimizers.Adam(lr=learning_rate, amsgrad=True))
-outputFolder = './output'
-if not os.path.exists(outputFolder):
-    os.makedirs(outputFolder)
-filepath=outputFolder+"/model-{epoch:02d}-{val_accuracy:.2f}.hdf5"
+def get_compiled_model():
+    model = Depth()
+    model.compile(loss=loss, optimizer=tf.keras.optimizers.Adam(lr=learning_rate, amsgrad=True))
+    return model
+    
+checkpoint_dir = "./ckpt"
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
 
-checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_weights_only=True, save_frequency=1)
+def make_or_restore_model():
+    checkpoints = [checkpoint_dir + "/" + name for name in os.listdir(checkpoint_dir)]
+    if checkpoints:
+        latest_checkpoint = max(checkpoints, key=os.path.getctime)
+        print("Restoring from", latest_checkpoint)
+        return keras.models.load_model(latest_checkpoint)
+    print("Creating a new model")
+    return get_compiled_model()
 
-model.fit(data_generator, epochs=epochs, steps_per_epoch=length//batch_size, callbacks=[checkpoint_callback])
+
+model = make_or_restore_model()
+callbacks = [
+    tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_dir + "/ckpt-loss={loss:.2f}", save_freq=1000, verbose=1 
+    )
+]
+
+
+model.fit(data_generator, epochs=epochs, steps_per_epoch=length//batch_size, callbacks=callbacks)

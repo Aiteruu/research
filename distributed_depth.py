c6191f4a3ba2e63af3b770dd2db6030ea0ad0f57
Aiteruu <42684804+Aiteruu@users.noreply.github.com>
2020-10-22 19:12:11 -0400

    add mirrored strategy

--- distributed_depth.py
+++ distributed_depth.py
@@ -4,15 +4,19 @@ from loss import loss
 import tensorflow as tf 
 import os
 
-batch_size = 4
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: %d' % strategy.num_replicas_in_sync)
+batch_size = 4 * strategy.num_replicas_in_sync
 learning_rate = 0.0001
 epochs = 10
 
 length, data_generator = batch(batch_size)
 
+
 def get_compiled_model():
-    model = Depth()
-    model.compile(loss=loss, optimizer=tf.keras.optimizers.Adam(lr=learning_rate, amsgrad=True))
+    with strategy.scope():
+        model = Depth()
+        model.compile(loss=loss, optimizer=tf.keras.optimizers.Adam(lr=learning_rate, amsgrad=True))
     return model
     
 checkpoint_dir = "./ckpt"
